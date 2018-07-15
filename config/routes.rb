2f44761f59847a286e312ffc2670d4bd873bd0c1
Oliver Hardman <oliver.hardman2@gmail.com>
2018-07-15 10:05:01 +1000

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root 'store#index'
+  root 'computers#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20180711060319_create_computers.rb
+++ db/migrate/20180711060319_create_computers.rb
@@ -2,7 +2,7 @@ class CreateComputers < ActiveRecord::Migration[5.2]
   def change
     create_table :computers do |t|
       t.string :title
-      t.decimal :price, precision: 5, scale: 2, default: 0
+      t.decimal :price, precision: 5, scale: 3, default: 0
       t.text :description
       t.string :finish
       t.string :case
