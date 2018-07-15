a5ea9895a66ee82a13fbf494d02d1651c01d7d41
Oliver Hardman <oliver.hardman2@gmail.com>
2018-07-16 08:40:36 +1000

    styled show page

--- app/helpers/computers_helper.rb
+++ app/helpers/computers_helper.rb
@@ -1,2 +1,7 @@
 module ComputersHelper
+
+    def computer_author(computer)
+        user_signed_in? && current_user.id == computer.user.id
+    end
+        
 end
--- app/models/computer.rb
+++ app/models/computer.rb
@@ -14,7 +14,7 @@ class Computer < ApplicationRecord
     GPU = %w{ Gigabyte_GTX_1080 NVIDIA_GTX_1080 ASUS_Radeon_RX_580 MSI_RX_VEGA_64}
     CPU = %w{ Intel_Core_i7-8700K AMD_Ryzen_2600 AMD_Threadripper_1950X Intel_Core_i9-7900X}
     HARDDRIVE = %w{ Samsung_860_Pro  Seagate_Barracuda_7200 Crucial_MX500}
-    MEMORY = %w{ Corsair_Vengeance GSkill_Ripjaws Kingston_HyperX_Fury }
+    MEMORY = %w{ Corsair Vengeance  GSkill Ripjaws  Kingston HyperX Fury }
     FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
     
 end
